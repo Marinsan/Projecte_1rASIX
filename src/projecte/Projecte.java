948bc266c64f49499c4455d1d527f0738e9feba8
marin <marin@marin-pc>
2018-03-20 09:41:53 +0100

    Menu ok menys esborrar

--- src/projecte/Projecte.java
+++ src/projecte/Projecte.java
@@ -17,19 +17,19 @@ public class Projecte {
         
         Scanner ent = new Scanner(System.in);
         
-        String model = null, nom = null;
-        int carregador = 0, menu;
-        double preu = 0.0;
-        boolean camuflatge = false, omplit = false;
-        char portacamuflatge = ' ', esS = ' ';
+        String nom = null, pais = null, director = null;
+        int menu, any = 0;
+        double nota = 0.0;
+        boolean estrena = false, omplit = false;
+        char estrenas = ' ', esS = ' ';
 
         do {
             System.out.println("\n\nMenú de l'aplicació:");
             System.out.println("0. Sortir");
-            System.out.println("1. Entrar arma");
-            System.out.println("2. Llistar armes");
-            System.out.println("3. Modificar arma");
-            System.out.println("4. Eliminar arma");
+            System.out.println("1. Entrar pel·lícula");
+            System.out.println("2. Llistar pel·lícules");
+            System.out.println("3. Modificar pel·lícula");
+            System.out.println("4. Eliminar pel·lícula");
             menu = ent.nextInt();
 
             switch (menu) {
@@ -40,19 +40,21 @@ public class Projecte {
                 case 1:
                     if (!omplit) {
                         System.out.println("Introdueix les dades");
-                        System.out.println("Model: ");
-                        model = ent.skip("[\r\n]*").nextLine();
                         System.out.println("Nom: ");
                         nom = ent.skip("[\r\n]*").nextLine();
-                        System.out.println("Carregador: ");
-                        carregador = ent.skip("[\r\n]*").nextInt();
-                        System.out.println("Preu:");
-                        preu = ent.skip("[\r\n]*").nextDouble();
-                        System.out.println("Camuflatge (Si/No): ");
+                        System.out.println("Pais: ");
+                        pais = ent.skip("[\r\n]*").nextLine();
+                        System.out.println("Director: ");
+                        director = ent.skip("[\r\n]*").nextLine();
+                        System.out.println("Any: ");
+                        any = ent.skip("[\r\n]*").nextInt();
+                        System.out.println("Nota:");
+                        nota = ent.skip("[\r\n]*").nextDouble();
+                        System.out.println("Estrena (Si/No): ");
                         do {
-                            portacamuflatge = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
-                        } while (portacamuflatge != 'S' && portacamuflatge != 'N');
-                        camuflatge = (portacamuflatge == 'S');
+                            estrenas = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
+                        } while (estrenas != 'S' && estrenas != 'N');
+                        estrena = (estrenas == 'S');
 
                         omplit = true;
                     } else {
@@ -62,46 +64,37 @@ public class Projecte {
 
                 case 2:
                     if (omplit == true) {
-                        System.out.println("\nModel: " + model);
-                        System.out.println("Nom: " + nom);
-                        System.out.println("Carregador: " + carregador);
-                        System.out.println("Preu:" + preu);
-                        System.out.println("Camuflatge (Si/No): " + camuflatge);
+                        System.out.println("\nNom: " + nom);
+                        System.out.println("Pais: " + pais);
+                        System.out.println("Dirctor: " + director);
+                        System.out.println("Nota:" + nota);
+                        System.out.println("Estrena (Si/No): " + estrena);
                     } else {
                         System.out.println("No hi han dades.");
                     }
                     break;
 
                 case 3:
-                    if (omplit = true) {
-                        System.out.println("\nVols vore les dades de les armes? (Si/No)");
+                    if (omplit == true) {
+                        System.out.println("\nVols vore les dades de les pel·lícules? (Si/No)");
                         do {
                             esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                         } while (esS != 'S' && esS != 'N');
                         if (esS == 'S') {
-                            System.out.println("Model: " + model);
-                            System.out.println("Nom: " + nom);
-                            System.out.println("Carregador: " + carregador);
-                            System.out.println("Preu:" + preu);
-                            System.out.println("Camuflatge (Si/No): " + camuflatge);
+                            System.out.println("\nNom: " + nom);
+                            System.out.println("Pais: " + pais);
+                            System.out.println("Dirctor: " + director);
+                            System.out.println("Nota:" + nota);
+                            System.out.println("Estrena (Si/No): " + estrena);
                         }
                         System.out.println("\nVols modificar les dades? (Si/No)");
                         do {
                             esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                         } while (esS != 'S' && esS != 'N');
                         if (esS == 'S') {
-                            System.out.println("\nModel: " + model);
+                            System.out.println("\nNom: " + nom);
                         }
-                        System.out.println("\nVols modificar el model? (Si/No)");
-                        do {
-                            esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
-                        } while (esS != 'S' && esS != 'N');
-                        if (esS == 'S') {
-                            System.out.println("Model: ");
-                            model = ent.skip("[\r\n]*").nextLine();
-                        }
-                        System.out.println("\nNom: " + nom);
-                        System.out.println("\nVols modificar el nom? (Si/No)");
+                        System.out.println("\nVols modificar la pel·lícula? (Si/No)");
                         do {
                             esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                         } while (esS != 'S' && esS != 'N');
@@ -109,69 +102,79 @@ public class Projecte {
                             System.out.println("Nom: ");
                             nom = ent.skip("[\r\n]*").nextLine();
                         }
-                        System.out.println("\nCarregador: " + carregador);
-                        System.out.println("\nVols modificar el carregador? (Si/No)");
+                        System.out.println("\nPais: " + pais);
+                        System.out.println("\nVols modificar el pais? (Si/No)");
                         do {
                             esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                         } while (esS != 'S' && esS != 'N');
                         if (esS == 'S') {
-                            System.out.println("Carregador: ");
-                            carregador = ent.skip("[\r\n]*").nextInt();
+                            System.out.println("Nom: ");
+                            nom = ent.skip("[\r\n]*").nextLine();
                         }
-                        System.out.println("\nPreu: " + preu);
-                        System.out.println("\nVols modificar el Preu? (Si/No)");
+                        System.out.println("\nDirector: " + director);
+                        System.out.println("\nVols modificar el director? (Si/No)");
                         do {
                             esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                         } while (esS != 'S' && esS != 'N');
                         if (esS == 'S') {
-                            System.out.println("Preu: ");
-                            preu = ent.skip("[\r\n]*").nextLine();
+                            System.out.println("Director: ");
+                            director = ent.skip("[\r\n]*").nextInt();
                         }
-                        System.out.println("Camuflatge (Si/No): ");
-                        System.out.println("\nVols modificar el camuflatge? (Si/No)");
+                        System.out.println("\nNota: " + nota);
+                        System.out.println("\nVols modificar la nota? (Si/No)");
                         do {
-                            portacamuflatge = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
-                        } while (portacamuflatge != 'S' && portacamuflatge != 'N');
-                        camuflatge = (portacamuflatge == 'S');
+                            esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
+                        } while (esS != 'S' && esS != 'N');
+                        if (esS == 'S') {
+                            System.out.println("Nota: ");
+                            nota = ent.skip("[\r\n]*").nextLine();
+                        }
+                        System.out.println("Estrena (Si/No): ");
+                        System.out.println("\nVols modificar l'estrena? (Si/No)");
+                        do {
+                            estrenas = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
+                        } while (estrenas != 'S' && estrenas != 'N');
+                        estrena = (estrenas == 'S');
 
                         omplit = true;
+                    }
 
                         break;
 
                     
             
-            case 4:
-                if (omplit = true) {
-                    System.out.println("\nVols vore les dades? (Si/No)");
+                case 4:
+                    if (omplit == true) {
+                        System.out.println("\nVols vore les dades? (Si/No)");
                     do {
                         esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                     } while (esS != 'S' && esS != 'N');
-                    if = (esS == 'S') {
-                     System.out.println("Model: " + model);
-                     System.out.println("Nom: " + nom);
-                     System.out.println("Carregador: " + carregador);
-                     System.out.println("Preu:" + preu);
-                     System.out.println("Camuflatge (Si/No): " + camuflatge);
+                    if  (esS == 'S') {
+                            System.out.println("\nNom: " + nom);
+                            System.out.println("Pais: " + pais);
+                            System.out.println("Dirctor: " + director);
+                            System.out.println("Nota:" + nota);
+                            System.out.println("Estrena (Si/No): " + estrena);
                     }
-                    System.out.println("\nSegur que vols borrar les dades? (Si/No)");
+                        System.out.println("\nSegur que vols borrar les dades? (Si/No)");
                     do {
                         esS = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
                     } while (esS != 'S' && esS != 'N');
-                    if = (esS == 'S') {
+                    if (esS == 'S') {
                         omlpit = false;
-                    System.out.println("\nDades borrades correctament");
-              }else{
-                    System.out.println("\nNo s'han borrat les dades");
-                    }
-            }else{
-                    System.out.println("\nNo hi han dades");
-           }
-            break;
+                        System.out.println("\nDades borrades correctament");
+                            }else{
+                        System.out.println("\nNo s'han borrat les dades");
+                                 }
+                            }else{
+                        System.out.println("\nNo hi han dades");
+                                 }
+                        break;
 
-          default:
-                    System.out.println("\nOpció erronea.");
-                    }                } 
-        } while (!(menu == 0));
+                        default:
+                        System.out.println("\nOpció erronea.");
+                                }                
+                                    } while (!(menu == 0));
 
         }
 

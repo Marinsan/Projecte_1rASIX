061e5832dcee95be82c2570fbc8c08a6cbae35f8
marin <marin@marin-pc>
2018-03-21 09:35:38 +0100

    Clase peli creada

---
+++ src/projecte/Peli.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecte;
+
+/**
+ *
+ * @author marin
+ */
+public class Peli {
+    
+        String nom = null, pais = null, director = null;
+        int menu, any = 0;
+        double nota = 0.0;
+        boolean estrena = false, omplit = false;
+        char estrenas = ' ', esS = ' ';
+    
+}
+
+
